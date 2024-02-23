3516f408a58faf38cdf3554e1e3ab3a0528df0bc
MusaAfandiyev <afandiyev.musa2003@gmail.com>
2024-02-23 20:39:47 +0400

    crud operationare here, only we need to write update

--- src/main/java/az/edu/ada/wm2/firstspringapp/FirstSpringAppApplication.java
+++ src/main/java/az/edu/ada/wm2/firstspringapp/FirstSpringAppApplication.java
@@ -9,29 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @SpringBootApplication
-@RestController // "/"
 public class FirstSpringAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstSpringAppApplication.class, args);
-	}
-
-	@GetMapping("/")
-	public String greetUser(){
-		return "Hello World!";
-	}
-
-	public List<String> getPeople(){
-		return List.of("Ali", "Musa", "Ilkin");
-	}
-
-	@GetMapping("/personObject")
-	public List<Person> getPersonObject(){
-		return List.of(
-				new Person("Musa", "Afandiyev"),
-				new Person("Zumrud", "Isgandarli"),
-				new Person("Fidan", "Yusifova")
-		);
 	}
 
 }
--- src/main/java/az/edu/ada/wm2/firstspringapp/controller/PersonController.java
+++ src/main/java/az/edu/ada/wm2/firstspringapp/controller/PersonController.java
@@ -1,33 +1,51 @@
 package az.edu.ada.wm2.firstspringapp.controller;
 
 import az.edu.ada.wm2.firstspringapp.model.Person;
+import az.edu.ada.wm2.firstspringapp.service.PersonService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.model.IModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import java.util.List;
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/persons")
+@RequiredArgsConstructor
 public class PersonController {
 
-    @Autowired
-    private Person defaultPerson;
+    private final PersonService personService;
 
-    @GetMapping
+    @GetMapping({"", "/"})
     public String listPersons(Model model){
-
-        System.out.println(defaultPerson);
-        var list = List.of(
-                new Person("Musa", "Afandiyev"),
-                new Person("Zumrud", "Isgandarli"),
-                new Person("Fidan", "Yusifova")
-        );
-
+        var list = personService.listPersons();
         model.addAttribute("persons", list);
         return "person_list";
     }
+
+    @GetMapping("/new")
+    public String newForm(Model model){
+        model.addAttribute("person", new Person("",""));
+        return "new_person_form";
+    }
+
+    @PostMapping("/save")
+    public String savePerson(Model model, @ModelAttribute("person") Person newPerson){
+        personService.addPerson(newPerson);
+        return "redirect:/persons";
+    }
+
+    @GetMapping("/delete/{idx}")
+    public String deletePerson(@PathVariable Integer idx ){
+//        System.out.println(idx);
+        personService.deletePerson(idx);
+        return "redirect:/persons";
+    }
 }
+
